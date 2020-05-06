b15477e441f97e24d72f73e17c4e99bfbc86daf6
MaciejLesniak97 <macieklesniak7@wp.pl>
2020-05-07 00:13:40 +0200

    Todo App second commit

---
+++ training/src/components/ContactCard.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function ContactCard(props) {
+    return (
+        <div>
+            <img scr={props.contact.imgUrl}/>
+            <h3>{props.contact.name}</h3>
+            <p>{props.contact.phone}</p>
+            <p>{props.contact.email}</p>
+        </div>
+    )
+}
+
+export default ContactCard
+
