383f0dd12186927a2dad5cc2ba41fb1b892a0c17
MaciejLesniak97 <macieklesniak7@wp.pl>
2020-05-07 20:39:40 +0200

    todo app 4 commit

--- training/src/components/TodoItem.js
+++ training/src/components/TodoItem.js
@@ -1,14 +1,20 @@
 import React from "react"
 
 function TodoItem(props){
+    const completedStyle = {
+        fontstyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return(
         <div className="todo-item">
             <input 
                 type = "checkbox" 
                 checked = {props.item.completed} 
-                onChange={(event) =>  props.handleChange(props.item.id)}
+                onChange={() =>  props.handleChange(props.item.id)}
             />
-            <p>{props.item.text}</p>
+            <p style = {props.item.completed ? completedStyle : null} >{props.item.text}</p>
         </div>
     )
 }
